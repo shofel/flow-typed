047240fbf95cdeb98b6afaf6135adabe2ce25103
shofel <v.istomin@examus.net>
2017-03-15 12:55:11 +0500

    ramda: refactor test for compose

--- definitions/npm/ramda_v0.23.x/flow_v0.41.x-/test_ramda_v0.23.x_Function_compose.js
+++ definitions/npm/ramda_v0.23.x/flow_v0.41.x-/test_ramda_v0.23.x_Function_compose.js
@@ -1,8 +1,6 @@
 // @flow
 import R from 'ramda'
 
-type Unary<T1, T2> = T1 => T2
-
 type A = 'type A'
 type B = 'type B'
 type C = 'type C'
@@ -10,26 +8,26 @@ type D = 'type D'
 type E = 'type E'
 type F = 'type F'
 
-declare var ab: Unary<A, B>
-declare var bc: Unary<B, C>
-declare var cd: Unary<C, D>
-declare var de: Unary<D, E>
-declare var ef: Unary<E, F>
+declare var ab: (A => B)
+declare var bc: (B => C)
+declare var cd: (C => D)
+declare var de: (D => E)
+declare var ef: (E => F)
 
 /* TODO: when the last fn is non-unary */
 
 /* works well with one fn */
 {
-  let f1: Unary<A, B> = R.compose(ab)
+  let f1: (A => B) = R.compose(ab)
   // $ExpectError
-  let f2: Unary<A, A> = R.compose(ab)
+  let f2: (A => A) = R.compose(ab)
 }
 
 /* two functions */
 {
-  let f1: Unary<A, C> = R.compose(bc, ab)
+  let f1: (A => C) = R.compose(bc, ab)
   // $ExpectError
-  let f2: Unary<A, B> = R.compose(bc, ab)
+  let f2: (A => B) = R.compose(bc, ab)
   // $ExpectError
   let f3 = R.compose(cd, ab)
   // $ExpectError
@@ -37,4 +35,7 @@ declare var ef: Unary<E, F>
 }
 
 /* three functions */
+{
+  let f1: (A => D) = R.compose(cd, bc, ab)
+}
 
